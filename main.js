9d9037e8fef4304ef4b97e733aa22d64ae49e3c2
Jainee <janey.wang0110@gmail.com>
2024-10-13 17:14:40 +0800

    Back to top bottom

--- main.js
+++ main.js
@@ -60,3 +60,25 @@ var swiper = new Swiper(".mySwiper", {
     },
   
   });
+
+
+const showOnPx = 2000;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+});
+
+const goToTop = () => {
+  document.body.scrollIntoView();
+};
+
+backToTopButton.addEventListener("click", goToTop);
